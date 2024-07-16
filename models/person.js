0e289175136bf20c0dfea71ba78836cdaa5c25d4
Oliver Saari <saarioliver02@gmail.com>
2024-07-16 17:24:37 +0300

    3.19-3.21, eslint installed

--- models/person.js
+++ models/person.js
@@ -2,13 +2,10 @@ const mongoose = require('mongoose')
 
 mongoose.set('strictQuery', false)
 
-
 const url = process.env.MONGODB_URI
-
 
 console.log('connecting to', url)
 mongoose.connect(url)
-
   .then(result => {
     console.log('connected to MongoDB')
   })
@@ -17,8 +14,21 @@ mongoose.connect(url)
   })
 
 const personSchema = new mongoose.Schema({
-  name: String,
-  number: String,
+  name: {
+    type: String,
+    minlength: 3,
+    required: true
+  },
+  number: {
+    type: String,
+    required: true,
+    validate : {
+      validator: function(v) {
+        return /^\d{2,3}-\d{5,}$/.test(v)
+      },
+      message: props => `${props.value} is not a valid phone number!`
+    }
+  }
 })
 
 personSchema.set('toJSON', {
